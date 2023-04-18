3867600c092fe6aedefd346152811f3c7c0acf3d
EstherWang0314 <xueyingw5@student.unimelb.edu.au>
2023-04-18 19:02:54 +1000

    debug

--- Flask/app.py
+++ Flask/app.py
@@ -109,4 +109,4 @@ class api_2(Resource):
 api.add_resource(api_2, '/api_2', '/api_2/<id>')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port='8080')
+    app.run(debug=True, host='172.26.130.118', port='8080')
